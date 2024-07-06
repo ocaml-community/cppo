a3e3728334389eae2dc4c86f9429a09c6377d88e
François Pottier <francois.pottier@inria.fr>
2024-07-06 14:23:39 +0200

    Cppo_eval.mli: make the type [env] abstract. Cppo_eval.ml:  make the type [env] an algebraic data type.
    This avoids some confusion, as the constructors [`Def] and [`Defun]
    were previously used in two different types, namely [node] and [env],
    with different arguments.

--- src/cppo_eval.mli
+++ src/cppo_eval.mli
@@ -5,13 +5,9 @@
 module S : Set.S with type elt = string
 module M : Map.S with type key = string
 
-val builtin_env
-  : [> `Defun of
-         Cppo_types.loc * string * string list *
-         [> `Capitalize of Cppo_types.node
-         | `Concat of (Cppo_types.node * Cppo_types.node)
-         | `Stringify of Cppo_types.node ] list * 'a
-    | `Special ] M.t as 'a
+type env
+
+val builtin_env : env
 
 val include_inputs
   : extensions:(string, Cppo_command.command_template) Hashtbl.t
@@ -20,10 +16,5 @@ val include_inputs
   -> show_exact_locations:bool
   -> show_no_locations:bool
   -> Buffer.t
-  -> (([< `Def of Cppo_types.loc * string * Cppo_types.node list * 'a
-       | `Defun of Cppo_types.loc * string * string list * Cppo_types.node list * 'a
-       | `Special
-            > `Def `Defun ]
-       as 'b)
-        M.t as 'a)
-  -> (string * string * (unit -> Lexing.lexbuf) * (unit -> unit)) list -> 'a
+  -> env
+  -> (string * string * (unit -> Lexing.lexbuf) * (unit -> unit)) list -> env
